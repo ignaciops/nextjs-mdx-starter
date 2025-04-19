a64b624d2a7d443979d89ed11952475de60893e8
Igancio PS <ignacio@perezsmith.me>
2025-04-19 15:51:00 -0600

    Removed Eslintrc from project

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,8 +1,12 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { Linter } from "eslint";
 import eslintPlugin from "@eslint/js";
+import tseslint, { configs as tseslintConfigs } from 'typescript-eslint';
+import type { FlatConfig } from '@typescript-eslint/utils/ts-eslint';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import jsxA11yPlugin from 'eslint-plugin-jsx-ally';
+import nextPlugin from '@next/eslint-plugin-next';
 
-const compat = new FlatCompat();
 
 const eslintConfig = [
   {
@@ -21,12 +25,87 @@ const ignoresConfig = [
       'public',
     ]
   },
-] as Linter.Config[]
+] as FlatConfig.Config[]
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+const tseslintConfig = tseslint.config(
+  {
+    name: 'custom/typescript-eslint/recommended',
+    files: ['**/*.mjs', '**/*.ts?(x)'],
+    extends: [
+      ...tseslintConfigs.recommended,
+      ...tseslintConfigs.stylistic,
+    ] as FlatConfig.ConfigArray,
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  },
+  {
+    files: ['**/*.mjs'],
+    ...tseslintConfigs.disableTypeChecked,
+    name: 'custom/typescript-eslint/disable-type-checked',
+  },
+)
+
+const nextConfig = [
+  {
+    name: 'custom/next/config',
+        // no files (option) for this config as we want to apply it to all files
+        plugins: {
+            'react': reactPlugin,
+            'jsx-a11y': jsxA11yPlugin,
+            /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+            'react-hooks': reactHooksPlugin,
+            '@next/next': nextPlugin,
+            'import': importPlugin,
+            /* eslint-enable @typescript-eslint/no-unsafe-assignment */
+        },
+        rules: {
+            ...reactPlugin.configs.recommended.rules,
+            ...reactPlugin.configs['jsx-runtime'].rules,
+            /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+            ...reactHooksPlugin.configs.recommended.rules,
+            ...nextPlugin.configs.recommended.rules,
+            // this is the nextjs strict mode
+            ...nextPlugin.configs['core-web-vitals'].rules,
+            ...importPlugin.configs.recommended.rules,
+            /* eslint-enable @typescript-eslint/no-unsafe-member-access */
+            //...jsxA11yPlugin.configs.recommended.rules,
+            // OR more strict a11y rules
+            ...jsxA11yPlugin.configs.strict.rules,
+            // rules from eslint-config-next
+            'import/no-anonymous-default-export': 'warn',
+            'react/no-unknown-property': 'off',
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react/jsx-no-target-blank': 'off',
+            'jsx-a11y/alt-text': ['warn', { elements: ['img'], img: ['Image'], },],
+            'jsx-a11y/aria-props': 'warn',
+            'jsx-a11y/aria-proptypes': 'warn',
+            'jsx-a11y/aria-unsupported-elements': 'warn',
+            'jsx-a11y/role-has-required-aria-props': 'warn',
+            'jsx-a11y/role-supports-aria-props': 'warn',
+        },
+        settings: {
+            'react': {
+                version: 'detect',
+            },
+            // only needed if you use (eslint-import-resolver-)typescript
+            'import/resolver': {
+                typescript: {
+                    alwaysTryTypes: true
+                }
+            }
+        },
+    }
+] as FlatConfig.Config[]
+
+export default [
   ...eslintConfig,
   ...ignoresConfig,
-] satisfies Linter.Config[];
-
-export default eslintConfig;
+  ...tseslintConfig,
+  ...nextConfig,
+] satisfies FlatConfig.Config[]
